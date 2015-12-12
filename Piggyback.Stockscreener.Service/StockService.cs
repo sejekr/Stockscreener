27d096a8c0f721da642172f89e4aaa2eaa117987
Jens Kristoffersson <jens.kristoffersson@gmail.com>
2015-12-12 23:42:05 +0100

    First add

---
+++ Piggyback.Stockscreener.Service/StockService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piggyback.Stockscreener.Service
+{
+  public class StockService : IStockService
+  {
+    public DateTime GetCurrentDate()
+    {
+      return DateTime.Now;
+    }
+  }
+}
---
+++ Piggyback.Stockscreener/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Practices.Unity;
+using Piggyback.Stockscreener.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piggyback.Stockscreener
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      using (IUnityContainer container = new UnityContainer())
+      {
+        container.RegisterType<IStockService, StockService>();
+
+        var stockService = container.Resolve<IStockService>();
+        Console.WriteLine("Date: " + stockService.GetCurrentDate());
+        Console.WriteLine("Press any key to exit");
+        Console.ReadLine();
+      }
+    }
+  }
+}
